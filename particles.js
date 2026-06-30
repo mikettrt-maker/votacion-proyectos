efbc70f809b4e93054bb9269d65b319fc509ec1c
mikettrt-maker <mikettrt@gmail.com>
2026-06-30 16:23:55 -0600

    Particulas se juntan al tocar con el dedo, se dispersan al soltar

--- particles.js
+++ particles.js
@@ -5,9 +5,9 @@
   document.body.insertBefore(c, document.body.firstChild)
 
   var ctx = c.getContext('2d')
-  var w, h, particles = [], mouse = { x: -9999, y: -9999 }
-  var COLORS = ['#c084fc', '#a855f7', '#7c3aed', '#a78bfa', '#f1c40f']
-  var COUNT = 60
+  var w, h, particles = [], mouse = { x: -9999, y: -9999, active: false }
+  var COLORS = ['#c084fc', '#a855f7', '#7c3aed', '#a78bfa', '#f1c40f', '#e879f9']
+  var COUNT = 80
 
   function resize() {
     w = c.width = window.innerWidth
@@ -17,46 +17,61 @@
   resize()
 
   function Particle() {
-    this.x = Math.random() * w
-    this.y = Math.random() * h
-    this.vx = (Math.random() - 0.5) * 0.4
-    this.vy = (Math.random() - 0.5) * 0.4
-    this.r = Math.random() * 3 + 1.5
+    this.reset()
+  }
+
+  Particle.prototype.reset = function() {
+    this.tx = Math.random() * w
+    this.ty = Math.random() * h
+    this.x = this.tx
+    this.y = this.ty
+    this.vx = 0
+    this.vy = 0
+    this.r = Math.random() * 3.5 + 2
     this.color = COLORS[Math.floor(Math.random() * COLORS.length)]
-    this.alpha = Math.random() * 0.5 + 0.2
+    this.alpha = Math.random() * 0.4 + 0.3
   }
 
   Particle.prototype.update = function() {
-    var dx = this.x - mouse.x
-    var dy = this.y - mouse.y
-    var dist = Math.sqrt(dx * dx + dy * dy)
+    var targetX, targetY
 
-    if (dist < 120) {
-      var force = (120 - dist) / 120
-      this.vx += (dx / dist) * force * 0.5
-      this.vy += (dy / dist) * force * 0.5
+    if (mouse.active) {
+      targetX = mouse.x + (Math.random() - 0.5) * 20
+      targetY = mouse.y + (Math.random() - 0.5) * 20
+    } else {
+      targetX = this.tx
+      targetY = this.ty
     }
 
-    this.vx *= 0.98
-    this.vy *= 0.98
+    var dx = targetX - this.x
+    var dy = targetY - this.y
+    var dist = Math.sqrt(dx * dx + dy * dy)
+
+    if (dist > 1) {
+      var speed = mouse.active ? 0.15 : 0.03
+      this.vx += (dx / dist) * speed
+      this.vy += (dy / dist) * speed
+    }
+
+    this.vx *= 0.9
+    this.vy *= 0.9
     this.x += this.vx
     this.y += this.vy
-
-    if (this.x < -20) this.x = w + 20
-    if (this.x > w + 20) this.x = -20
-    if (this.y < -20) this.y = h + 20
-    if (this.y > h + 20) this.y = -20
   }
 
   Particle.prototype.draw = function() {
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2)
+
+    var grad = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.r * 2)
+    grad.addColorStop(0, this.color)
+    grad.addColorStop(1, this.color + '00')
+
     ctx.fillStyle = this.color
     ctx.globalAlpha = this.alpha
-    ctx.fill()
 
     ctx.shadowColor = this.color
-    ctx.shadowBlur = 8
+    ctx.shadowBlur = mouse.active ? 20 : 6
     ctx.fill()
     ctx.shadowBlur = 0
     ctx.globalAlpha = 1
@@ -74,13 +89,13 @@
         var dx = particles[i].x - particles[j].x
         var dy = particles[i].y - particles[j].y
         var dist = Math.sqrt(dx * dx + dy * dy)
-        if (dist < 100) {
+        if (dist < 120) {
           ctx.beginPath()
           ctx.moveTo(particles[i].x, particles[i].y)
           ctx.lineTo(particles[j].x, particles[j].y)
           ctx.strokeStyle = particles[i].color
-          ctx.globalAlpha = (1 - dist / 100) * 0.15
-          ctx.lineWidth = 0.5
+          ctx.globalAlpha = (1 - dist / 120) * (mouse.active ? 0.3 : 0.08)
+          ctx.lineWidth = mouse.active ? 1 : 0.5
           ctx.stroke()
           ctx.globalAlpha = 1
         }
@@ -99,24 +114,14 @@
   }
   animate()
 
-  var throttle = false
-  function onMove(e) {
-    if (throttle) return
-    throttle = true
-    setTimeout(function(){ throttle = false }, 30)
+  document.addEventListener('mousedown', function(e){ mouse.x = e.clientX; mouse.y = e.clientY; mouse.active = true })
+  document.addEventListener('mousemove', function(e){ if (mouse.active) { mouse.x = e.clientX; mouse.y = e.clientY } })
+  document.addEventListener('mouseup', function(){ mouse.active = false; mouse.x = -9999; mouse.y = -9999 })
+  document.addEventListener('mouseleave', function(){ mouse.active = false; mouse.x = -9999; mouse.y = -9999 })
 
-    var p = e.touches ? e.touches[0] : e
-    mouse.x = p.clientX
-    mouse.y = p.clientY
-  }
+  document.addEventListener('touchstart', function(e){ var t = e.touches[0]; mouse.x = t.clientX; mouse.y = t.clientY; mouse.active = true })
+  document.addEventListener('touchmove', function(e){ var t = e.touches[0]; mouse.x = t.clientX; mouse.y = t.clientY; mouse.active = true })
+  document.addEventListener('touchend', function(){ mouse.active = false; mouse.x = -9999; mouse.y = -9999 })
 
-  document.addEventListener('mousemove', onMove)
-  document.addEventListener('touchmove', onMove)
-  document.addEventListener('touchstart', onMove)
-  document.addEventListener('mouseleave', function(){ mouse.x = -9999; mouse.y = -9999 })
-
-  window.addEventListener('resize', function(){
-    resize()
-    init()
-  })
+  window.addEventListener('resize', function(){ resize(); init() })
 })()
